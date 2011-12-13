4bdf12a9bedc9c50f2c3e92e052d569451820295
unknown <AVT@AVTCOMP.(none)>
2011-12-13 20:55:53 +0600

    First commit

---
+++ src/com/avt/poslovchik/PoslovchikActivity.java
@@ -0,0 +1,15 @@
+package com.avt.poslovchik;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PoslovchikActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
